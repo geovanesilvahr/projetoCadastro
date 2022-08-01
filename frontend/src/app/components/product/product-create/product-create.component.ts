73a712a21bc7d447d7faaa96ec927d0447914fc7
Geovane Silva <geovanesilva.hr@gmail.com>
2022-07-31 23:34:48 -0300

    Including Products Components - Delete, Update and others upgrades.

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    name: '',
+    price: 0.0
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Criado!")
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
